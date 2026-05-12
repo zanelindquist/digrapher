5bc3f8c8cda188865ae1198593d6323eb5b94977
Zane Lindquist <ztlindquist17@gmail.com>
2026-05-12 17:20:58 -0600

    feat: Tool preview cards on home page

--- src/components/misc/tool_preview.rs
+++ src/components/misc/tool_preview.rs
@@ -1,4 +1,7 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+use crate::router::Route;
 
 #[derive(Properties, Clone, PartialEq, serde::Deserialize, serde::Serialize)]
 pub struct ToolData {
@@ -18,13 +21,22 @@ pub struct ToolPreviewProps {
 
 #[function_component(ToolPreview)]
 pub fn logo(props: &ToolPreviewProps) -> Html {
+    let route = match props.tool_data.name.to_lowercase().as_str() {
+        "digraph" => Route::Digraph,
+        _ => Route::NotFound
+    };
 
     html!{
         <div class={classes!("tool-preview", props.class.clone())}>
-            <img class="tool-preview__img" src={format!("../../assets/tool_preview_images/{}", props.tool_data.image_path)}/>
-            <code class="tool-preview__name">{props.tool_data.name.clone()}</code>
-            <code class="tool-preview__description">{props.tool_data.description.clone()}</code>
-            <code class="tool-preview__date">{format!("{}", props.tool_data.published_date)}</code>
+            <Link<Route>
+                to={route}
+                classes="tool-preview__link"
+            >
+                <img class="tool-preview__img" src={format!("/assets/tool_preview_images/{}", props.tool_data.image_path)}/>
+                <code class="tool-preview__name">{props.tool_data.name.clone()}</code>
+                <code class="tool-preview__description">{props.tool_data.description.clone()}</code>
+                <code class="tool-preview__date">{format!("{}", props.tool_data.published_date)}</code>
+            </Link<Route>>
         </div>
     }
 }
