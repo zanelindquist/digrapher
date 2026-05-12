885d638b2a1f132e3b4df866cd623402cd9df473
Zane Lindquist <ztlindquist17@gmail.com>
2026-05-12 17:39:04 -0600

    fix: Scolling UI

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -1,10 +1,15 @@
 use yew::prelude::*;
 
+use crate::components::navigation::topbar_layout::TopbarLayout;
+
 #[function_component(NotFoundPage)]
 pub fn not_found_page() -> Html {
     html! {
-        <div>
-            <h1>{ "404 Not found" }</h1>
-        </div>
+        <TopbarLayout>
+            <div class="not-found">
+                <h1 class="not-found__heading">{ "404 Not found" }</h1>
+                <code class="not-found__description">{ "The page you were looking for could not be located." }</code>
+            </div>
+        </TopbarLayout>
     }
 }
