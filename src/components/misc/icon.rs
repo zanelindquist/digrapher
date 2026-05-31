4341d8aed7aa8222603c9f8f2ad82622ef3f5e85
Zane Lindquist <ztlindquist17@gmail.com>
2026-05-31 14:22:28 -0600

    feat: Toggle sidebar

--- src/components/misc/icon.rs
+++ src/components/misc/icon.rs
@@ -53,6 +53,12 @@ pub fn icon(props: &IconProps) -> Html {
         "move" => html! {
             <path d="M13,6V11H18V7.75L22.25,12L18,16.25V13H13V18H16.25L12,22.25L7.75,18H11V13H6V16.25L1.75,12L6,7.75V11H11V6H7.75L12,1.75L16.25,6H13Z" />
         },
+        "arrow-collapse-right" => html! {
+            <path d="M12.08,4.08L20,12L12.08,19.92L10.67,18.5L16.17,13H2V11H16.17L10.67,5.5L12.08,4.08M20,12V22H22V2H20V12Z" />
+        },
+        "arrow-collapse-left" => html! {
+            <path d="M11.92,19.92L4,12L11.92,4.08L13.33,5.5L7.83,11H22V13H7.83L13.34,18.5L11.92,19.92M4,12V2H2V22H4V12Z" />
+        },
         _ => html! {
             <path />
         }, // fallback
